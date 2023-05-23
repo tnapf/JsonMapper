e696b300c37c3e42b5e0ef0008bc3f7462bc11b5
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-22 23:32:21 -0400

    Updated SnakeToCamelCase implementation

--- src/Attributes/SnakeToCamelCase.php
+++ src/Attributes/SnakeToCamelCase.php
@@ -5,15 +5,15 @@ namespace Tnapf\JsonMapper\Attributes;
 use Attribute;
 
 #[Attribute]
-class SnakeToCamelCase extends BaseClassAttribute
+class SnakeToCamelCase implements CaseConversionInterface
 {
-    public static function camelCaseToSnakeCase(string $input): string
-    {
-        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $input));
-    }
-
-    public static function snakeCaseToCamelCase(string $input): string
+    public function convertToCase(string $input): string
     {
         return lcfirst(str_replace('_', '', ucwords($input, '_')));
+    }
+
+    public function convertFromCase(string $input): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $input));
     }
 }
